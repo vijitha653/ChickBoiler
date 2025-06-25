c5f35190a122d6adaa8f36d06b40d5798099773e
vijitha653 <shreevijithak@gmail.com>
2025-06-25 16:55:00 +0530

    favicon

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { View, Text, StyleSheet } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text> ChickBoiler</Text>
+      <Text> ChickBoilerr</Text>
     </View>
   );
 }
